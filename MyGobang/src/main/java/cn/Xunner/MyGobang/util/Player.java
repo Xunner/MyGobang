edc896f879bf2bf915fca70772f4c8784d2567bb
巽 <161250042@mail.smal.nju.edu.cn>
2018-01-25 22:52:23 +0800

    feat: create Player enum

---
+++ MyGobang/src/main/java/cn/Xunner/MyGobang/util/Player.java
@@ -0,0 +1,21 @@
+package cn.Xunner.MyGobang.util;
+
+/**
+ * Created on 2018/1/25
+ * 
+ * @author 巽
+ *
+ */
+public enum Player {
+	BLACK("黑方"), WHITE("白方");
+
+	String value;
+
+	private Player(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return this.value;
+	}
+}
