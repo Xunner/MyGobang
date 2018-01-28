50a1dada8d51b16a9f48bd856d65b5fccb2cd5d3
Xunner <419848067@qq.com>
2018-01-28 23:29:26 +0800

    feat: create game basic ui

---
+++ MyGobang/src/main/java/cn/Xunner/MyGobang/ui/GameController.java
@@ -0,0 +1,30 @@
+package cn.Xunner.MyGobang.ui;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.layout.HBox;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+import javafx.scene.layout.Pane;
+
+/**
+ * Created on 2018/1/28
+ * 
+ * @author 巽
+ *
+ */
+public class GameController {
+	@FXML
+	private HBox hBox;
+	@FXML
+	private AnchorPane leftPane;
+	@FXML
+	private Pane gamePane;
+	@FXML
+	private AnchorPane rightPane;
+
+	public void clickExitButton(MouseEvent mouseEvent){
+		// TODO
+	}
+}
